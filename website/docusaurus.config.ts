353f434005aff82b5c764bbda0fc1142e366ba30
Muhammad Haris Awan <160159398+harisawan27@users.noreply.github.com>
2025-12-06 00:04:00 +0500

    Update production URL from GitHub to Vercel

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://vercel.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: '/',
 
